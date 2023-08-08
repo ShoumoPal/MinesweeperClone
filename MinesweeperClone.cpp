9e5553474d044057033c1abdcb8eb2c20f1b08ca
ShoumoPal <arka.pal21@gmail.com>
2023-08-08 16:16:13 +0530

    Added printBoard function and the isMine check for mine checking

--- MinesweeperClone.cpp
+++ MinesweeperClone.cpp
@@ -4,13 +4,39 @@ using namespace std;
 
 #define MAXSIDE 25
 
-int side;
-int mines;
 
-bool isValid(int _row, int _col) {
-	return (((_row >= 0) && (_row < side)) && (_col >= 0) && (_col < side));
-}
 
+class MineSweeperGame {
+private:
+	int side;
+	int mines;
+	char board[][MAXSIDE];
+
+public:
+	// Function to validate user input
+	bool isValid(int _row, int _col) {
+		return (((_row >= 0) && (_row < side)) && (_col >= 0) && (_col < side));
+	}
+	// Function to validate mine at position given
+	bool isMine(int _row, int _col, char board[][MAXSIDE]) {
+		return board[_row][_col] == '*';
+	}
+	// Function to print the board
+	void printBoard(char board[][MAXSIDE]) {
+		cout << "     ";
+		for (int i = 0; i < side; i++) {
+			cout << i;
+		}
+		cout << "\n\n";
+		for (int i = 0; i < side; i++) {
+			cout << i << "   ";
+			for (int j = 0; j < side; j++) {
+				cout << board[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}	
+};
 int main() {
 	cout << "Hello";
 	return 0;
